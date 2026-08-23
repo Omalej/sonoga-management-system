434a0dcb02df11fac0cf95351dbccf0f662d111d
Sonoga CEO <ceo@sonogahotels.com>
2026-08-23 18:41:16 +0200

    Disable force password change middleware

--- sonoga_hms/settings.py
+++ sonoga_hms/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "accounts.middleware.ForcePasswordChangeMiddleware",
+    #"accounts.middleware.ForcePasswordChangeMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
